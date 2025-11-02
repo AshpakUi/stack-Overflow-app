235eaaff3a2d75d557e89cddfa2d1d8c8186bf86
Ashpakdev <109664129+AshpakUi@users.noreply.github.com>
2025-11-03 03:09:16 +0530

    Update index.js
    copy pasting index.js new file

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use("/user", userRoutes);
 app.use("/questions",questionRoutes);
 app.use("/answer",ansRoutes)
 
-const PORT =process.env.PORT||7000;
+const PORT =process.env.PORT||5000;
 const DATABASE_URL =process.env.CONNECTION_URL;
 mongoose
   .connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -31,4 +31,3 @@ mongoose
     })
   )
   .catch((error) => console.log(error.message));
-
